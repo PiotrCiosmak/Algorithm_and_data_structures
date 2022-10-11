8cd76391616f85d3bae06a8884831b61dd96d425
PiotrCiosmak <piotrciosmak2001@gmail.com>
2022-10-11 14:32:52 +0200

    BinarySearch done

--- src/BinarySearch/BinarySearch.java
+++ src/BinarySearch/BinarySearch.java
@@ -2,22 +2,32 @@ package BinarySearch;
 
 import java.util.List;
 
-public class BinarySearch {
+public class BinarySearch
+{
 
-    public static void main(String... args) {
+    public static void main(String... args)
+    {
         final var data = List.of("Adamiak", "Bór", "Cionowski", "Fabiński", "Gil", "Gralewska", "Jędrzejczyk",
                 "Jodłowski", "Kapustka", "Krajewski", "Lewandowski", "Lin", "Maran", "Milewski",
                 "Parański", "Perska", "Piszcz", "Salamandra", "Starzyński", "Stępiński",
                 "Warzywny", "Zieliński");
-        binarySearch(data, 0, data.size() - 1, "Krajewski");
+        binarySearch(data, 0, data.size() - 1, "12");
     }
 
-    private static int binarySearch(final List<String> data, final int leftEnd, final int rightEnd, final String value) {
+    private static int binarySearch(final List<String> data, final int leftEnd, final int rightEnd, final String value)
+    {
         final int mid = (leftEnd + rightEnd) / 2;
         System.out.printf("Current value: %s index: %s%n", data.get(mid), mid);
-        if (value.compareTo(data.get(mid)) < 0) {
+        if (value.compareTo(data.get(mid)) != 0 && (leftEnd + 1 == rightEnd))
+        {
+            return -1;
+        }
+        else if (value.compareTo(data.get(mid)) < 0)
+        {
             return binarySearch(data, leftEnd, mid, value);
-        } else if (value.compareTo(data.get(mid)) > 0) {
+        }
+        else if (value.compareTo(data.get(mid)) > 0)
+        {
             return binarySearch(data, mid, rightEnd, value);
         }
         return mid;
